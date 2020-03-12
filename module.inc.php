48329b5a014ce7399c5b1cc23df7d8ab6cd5d151
Manuel Zavatta <manuel.zavatta@gmail.com>
2020-03-12 10:41:38 +0100

    Module information

---
+++ module.inc.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Panel
+ *
+ * @package Coordinator\Modules\Panel
+ * @author  Manuel Zavatta <manuel.zavatta@gmail.com>
+ * @link    http://www.coordinator.it
+ */
+ // definitions
+ $module_name="panel";
+ $module_repository_url="https://github.com/Zavy86/Coordinator-Panel/";
+ $module_repository_version_url="https://raw.githubusercontent.com/Zavy86/Coordinator-Panel/master/VERSION.txt";
+ $module_required_modules=array();
+?>
